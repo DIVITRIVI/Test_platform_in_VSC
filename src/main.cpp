c9932f2bd4bdba287c7eb911be66f06f6f3663f7
DIVITRIVI <TheCVINFECT2@gmail.com>
2020-05-19 23:29:05 -0500

    Eliminar lineas
    No necesitamos tantas lineas de código

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,6 @@ void loop() {
   Serial.println("Este era la linea local");
   delay(1000);
   digitalWrite(led,LOW);
-  Serial.println("Agregando un cambio desde el entorno de GitHub");
-  delay(1000);
-  digitalWrite(led,HIGH);
-  Serial.println("Ahora este es un cambio local");
-  delay(1000);
-  digitalWrite(led,LOW);
-  Serial.println("Y para terminar, este es un cambio desde la web");
+  Serial.println("Y estoy realizando pruebas experimentales");
   delay(1000);
 }
