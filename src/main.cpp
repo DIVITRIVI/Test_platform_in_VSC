c893d18c5ab7fabaa4e06d702bf1e04a9c9e07f1
DIVITRIVI <TheCVINFECT2@gmail.com>
2020-05-19 23:37:09 -0500

    creo que 4 lineas podria servir

--- src/main.cpp
+++ src/main.cpp
@@ -13,4 +13,10 @@ void loop() {
   digitalWrite(led,LOW);
   Serial.println("Y estoy realizando pruebas experimentales");
   delay(1000);
+  digitalWrite(led,HIGH);
+  Serial.println("aunque esto cambios ya no se van a implementar");
+  delay(1000);
+  digitalWrite(led,LOW);
+  Serial.println("quiero ver como funciona el sistema");
+  delay(1000);
 }
