4965a06828613cb8db2a4a27d0b0b7b5c84ba386
DIVITRIVI <TheCVINFECT2@gmail.com>
2020-05-19 21:43:13 -0500

    agregando el directoro src

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#define led 13
+
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(1000);
+  Serial.println("Yeeee");
+}
